65332009f2d9520684c886a114026413a14bc335
Thomas-Galbignani <thomas.lino.galbignani@gmail.com>
2025-10-24 11:30:27 +0200

    Initialized Spring Project

---
+++ src/test/java/thomasgalbignani/BE_U2_Week3_Final_Project/BeU2Week3FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasgalbignani.BE_U2_Week3_Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2Week3FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
